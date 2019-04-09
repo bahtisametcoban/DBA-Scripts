28ef6ed29bba0196da10f5e99aaec331f52767a6
Samet Coban (NL-Limited_Admin) <adm-scoban@nl.flowtraders.local>
2019-04-09 12:53:15 +0200

    Trying to get rid of binary files

---
+++ Policy_ViewPolicyAndCategories.sql
@@ -0,0 +1,17 @@
+SELECT [policy_id]
+      , b.name 
+      , a.[name] 
+      , [date_created] 
+      , [description]
+      , [help_text] 
+      , [is_enabled]
+      , [job_id]
+      , [created_by]
+      , [modified_by]
+      , [date_modified]
+      , [is_system]
+FROM [msdb].[dbo].[syspolicy_policies] a
+    left join [msdb].[dbo].[syspolicy_policy_categories] b
+    on a.policy_category_id=b.policy_category_id
+WHERE b.name<>'DISABLED'
+order by b.name
